74cda9ed1c24640764b61735caad226ccbf60d45
singhmayank1298 <108435481+singhmayank1298@users.noreply.github.com>
2022-08-01 17:35:33 +0530

    curring

---
+++ index1.js
@@ -0,0 +1,29 @@
+
+//curring 
+let multiply=function(x,y){
+    console.log(x*y)
+}
+
+let multiplyby2=multiply.bind(this,2)
+multiplyby2(4)
+
+let multiplyby3=multiply.bind(this,3)
+multiplyby3(3)
+
+
+
+
+///using clouser
+
+let multiply1=function (x){
+    return function mul(y){
+        console.log( x*y)
+    }
+}
+
+let a= multiply1(2)
+   a(8)
+
+   let b= multiply1(3)
+   b(8)
+ //multiply1(1,2)
